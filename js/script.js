d0f543216288967cdd89254339cdd8c53e2d5699
Vince Aratan <vince.carl.aratan@aclcbutuan.edu.ph>
2022-09-22 10:18:29 +0800

    READ

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+let navbarDiv = document.querySelector('.navbar');
+window.addEventListener('scroll', () => {
+    if(document.body.scrollTop > 40 || document.documentElement.scrollTop > 40){
+        navbarDiv.classList.add('navbar-cng');
+    } else {
+        navbarDiv.classList.remove('navbar-cng');
+    }
+});
+const navbarCollapseDiv = document.getElementById('navbar-collapse');
+const navbarShowBtn = document.getElementById('navbar-show-btn');
+const navbarCloseBtn = document.getElementById('navbar-close-btn');
+
+navbarShowBtn.addEventListener('click', () => {
+    navbarCollapseDiv.classList.add('navbar-collapse-rmw');
+});
+
+document.addEventListener('click', (e) => {
+    if(e.target.id != "navbar-collapse" && e.target.id != "navbar-show-btn" && e.target.parentElement.id != "navbar-show-btn"){
+        navbarCollapseDiv.classList.remove('navbar-collapse-rmw');
+    }
+});
+
